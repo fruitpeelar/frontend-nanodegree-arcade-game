50e26b89a5512d7bfa6904b08fcfd1f3865df119
GY <fruitpeelar@gmail.com>
2016-04-13 03:14:05 -0700

    -instantiate allEnemies and player -create stubs for player constructor and functions -now the background of the game renders in the browser

--- js/app.js
+++ js/app.js
@@ -24,12 +24,26 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+	this.sprite = 'images/char-cat-girl.png';
+};
 
+Player.prototype.render = function() {
+	stx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.update = function() {
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+for (i = 0; i < 5; i++) {
+	allEnemies.push(new Enemy());
+}
 // Place the player object in a variable called player
-
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
